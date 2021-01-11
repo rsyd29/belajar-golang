c8ed94eefd6c2f2e79396c2d8dd48d7abd9b7fca
rsyd29 <brasyid15@gmail.com>
2021-01-11 16:47:13 +0700

    Video 22 tentang function return value telah selesai

---
+++ 22. Function Return Value/function-return-value.go
@@ -0,0 +1,43 @@
+/*
+	Function Return Value
+	1. Function bisa mengembalikan data
+	2. Untuk memberitahu bahwa function mengembalikan data,
+	kita harus menuliskan tipe dat akembalian dari function
+	tersebut
+	3. Jika function tersebut kita deklarasikan dengan tipe data
+	pengembalian, maka wajib di dalam function nya kita harus
+	mengembalikan data
+	4. Untuk mengembalikan data dari function, kita bisa
+	menggunakan kata kunci return, diikuti dengan datanya
+*/
+
+package main
+
+import "fmt"
+
+func getHello(name string) string {
+	// ini akan mengembalikan data yang tipe datanya string
+	// biasanya return di simpan pada bagian bahwa, setelah return kode tersebut akan kembali lagi ke functionnya
+	// Jadi kode blok di bawah return tidak akan pernah dieksekusi lagi
+	return "Hello " + name
+}
+
+/*
+ Kita juga bisa menggunakan return di dalam kondisi
+*/
+func getHelloBaru(name string) string {
+	if name == "Budi" {
+		return "Hello Budi" // return di dalam kondisi
+	} else {
+		return "Hello " + name
+	}
+}
+
+func main() {
+	// ketika kita memanggil func getHello maka itu akan mengembalikan data
+	// oleh karena itu kita bisa menyimpan datanya ke dalam variable
+	result := getHello("Budiman")
+	resultBaru := getHelloBaru("Rasyid")
+	fmt.Println(result)
+	fmt.Println(resultBaru)
+}
