6b4b26086c4ffb5e967b1cc744a4b9f16546f6d8
rsyd29 <brasyid15@gmail.com>
2021-01-14 15:07:57 +0700

    Video 32 tentang komentar telah selesai

---
+++ 32. Komentar/comment.go
@@ -0,0 +1,31 @@
+/*
+	Komentar
+	1. Komentar terbaik pada kode adalah kode itu sendiri
+	(kalau bisa kita harus membuat kode program yang
+	mudah dibaca oleh programmer lain)
+	2. Saat membuat kode, kita perlu membuat kode semudah
+	mungkin untuk dibaca
+	3. Namun kadang juga kita butuh menambahkan komentar
+	di kode kita
+*/
+
+package main
+
+/*
+this is multi line comment
+You can add a lot of comments here
+
+kalau multi line biasanya berada di atas function
+bertujuan untuk mendokumentasikan function ini untuk apa
+*/
+func main() {
+	// this is single line comment
+	// ini adalah komentar untuk satu baris
+	// single line biasanya digunakan untuk di blok function
+}
+
+/*
+pastikan ketika membuat program itu mudah untuk dibaca,
+mulai dari penamaan function, variable, dan apapun.
+Agar mudah dimengerti oleh programmer lain
+*/
