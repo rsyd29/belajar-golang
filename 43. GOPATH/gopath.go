a8491572fe28bf975412ce97c73f2b7a5d98fc40
rsyd29 <brasyid15@gmail.com>
2021-01-17 18:10:03 +0700

    Video 43 tentang GOPATH telah selesai

---
+++ 43. GOPATH/gopath.go
@@ -0,0 +1,18 @@
+/**
+GoPath
+1. Sebelumnya saat kita membuat project aplikasi Go-Lang kita belum membahas tentang GOPATH
+2. GOPATH adalah environment variable yang berisikan lokasi tempat kita menyimpan project dan
+library Go-Lang
+3. GOPATH wajib di buat ketika kita mulai membuat aplikasi lebih dari satu file atau butuh
+menggunakan library dari luar
+(istilahnya import)
+
+folder yang ada pada GOPATH
+1. src --> sebagai source untuk sumber kode program yang kita buat
+*/
+
+package main
+
+func main() {
+
+}
