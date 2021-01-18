eebaed65311eb357473eda96febd02697b921883
rsyd29 <brasyid15@gmail.com>
2021-01-18 09:28:12 +0700

    Updated video 44

--- 44-Package-dan-Import/import.go
+++ 44-Package-dan-Import/import.go
@@ -1,7 +1,8 @@
 package main
 
-import "belajar-golang/44. Package dan Import/helper"
-import "belajar-golang/44. Package dan Import/other"
+import "belajar-golang/44-Package-dan-Import/helper"
+import "belajar-golang/44-Package-dan-Import/other"
+
 // nama foldernya harus sesuaikan dengan gopathnya
 // ~/go/src/belajar-golang
 
