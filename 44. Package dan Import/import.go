ebb7c34ecc781a4b7bc0b972e0b502f5383c6e02
rsyd29 <brasyid15@gmail.com>
2021-01-18 09:19:16 +0700

    Video 44 tentang package dan import telah selesai

---
+++ 44. Package dan Import/import.go
@@ -0,0 +1,11 @@
+package main
+
+import "belajar-golang/44. Package dan Import/helper"
+import "belajar-golang/44. Package dan Import/other"
+// nama foldernya harus sesuaikan dengan gopathnya
+// ~/go/src/belajar-golang
+
+func main() {
+	helper.SayHello("Budiman")
+	other.SayHello("Rasyid")
+}
